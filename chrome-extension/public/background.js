9b2fb4bdf696b86a40c14409baf8b504e0d42dc4
Sung Un Kim <sungunkim367@gmail.com>
2019-03-12 00:23:19 +0900

    promisify background functions, add borders to form controls

--- chrome-extension/public/background.js
+++ chrome-extension/public/background.js
@@ -1,37 +1,38 @@
 /*global chrome*/
 
-// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-//   switch (message.action) {
-//     case 'popupOpen': {
-//       console.log('popup is open...');
-//       chrome.storage.local.get(['user'], function(response) {
-//         if (!response.user) {
-//           chrome.identity.getProfileUserInfo(function(result) {
-//             validateEmail(result.email);
-//             chrome.storage.local.set({
-//               resumeCount: 0,
-//               mailCount: 0,
-//               smsCount: 0
-//             });
-//           });
-//         }
-//       });
-//       break;
-//     }
-//     default: {
-//       console.log('no popup');
-//     }
-//   }
-// });
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  switch (message.action) {
+    case 'popupOpen': {
+      console.log('popup is open...');
+      chrome.storage.local.get(['user'], function(response) {
+        if (!response.user) {
+          chrome.identity.getProfileUserInfo(function(result) {
+            validateEmail(result.email);
+            chrome.storage.local.set({
+              resumeCount: 0,
+              mailCount: 0,
+              smsCount: 0,
+              user: result.user
+            });
+          });
+        }
+      });
+      break;
+    }
+    default: {
+      console.log('no popup');
+    }
+  }
+});
 
 chrome.extension.onConnect.addListener(function(port) {
   port.onMessage.addListener(async function(msg) {
     console.log('Received: ' + msg);
     if (msg === 'Requesting crawling') {
       let user = '';
-      chrome.storage.local.get(['user'], async function(response) {
+      await chrome.storage.local.get(['user'], async function(response) {
         if (!response.user) {
-          chrome.identity.getProfileUserInfo(async function(result) {
+          await chrome.identity.getProfileUserInfo(async function(result) {
             await chrome.storage.local.set({
               resumeCount: 0,
               mailCount: 0,
@@ -44,29 +45,26 @@ chrome.extension.onConnect.addListener(function(port) {
           user = response.user;
           console.log('user is already logged in.');
         }
-        getURL(_getValue);
-        getHTML(_getValue);
-        getHistory();
-        crawlCandidate();
 
-        await chrome.storage.local.get(null, function(response) {
-          sleep(5);
-          port.postMessage({
-            user: response.user,
-            url: response.url,
-            html: response.html,
-            history: response.history,
-            resumeCount: response.resumeCount,
-            candidate: response.candidate
-          });
-        });
+        await getURL();
+        await getHTML();
+        await getHistory();
+        await crawlCandidate();
+        await compileMessage(port);
+        // chrome.storage.local.get(null, response => {
+        //   console.log(response);
+        //   port.postMessage(response);
+        // });
       });
     } else if (msg === 'Requesting reset')
-      chrome.storage.local.set({
-        resumeCount: 0,
-        mailCount: 0,
-        smsCount: 0
-      });
+      chrome.storage.local.set(
+        {
+          resumeCount: 0,
+          mailCount: 0,
+          smsCount: 0
+        },
+        response => console.log(response)
+      );
   });
 });
 
@@ -93,84 +91,99 @@ function validateEmail(email) {
     .catch(error => console.log(error));
 }
 
-async function _getValue(param, value) {
-  console.log({ [param]: value });
-  await chrome.storage.local.set({ [param]: value });
-}
-
-const getURL = async callback => {
-  await chrome.tabs.query(
-    { active: true, currentWindow: true },
-    async ([currentTab]) => {
-      await callback('url', currentTab.url);
-    }
-  );
-};
-
-const getHTML = async callback => {
-  await chrome.tabs.executeScript(
-    null,
-    { code: 'var html = document.documentElement.outerHTML; html' },
-    async function(html) {
-      await callback('html', html[0]);
-    }
-  );
-};
-
-function getHistory() {
-  chrome.storage.local.get(null, async function(response) {
-    const api = 'http://128.199.203.161:8500/extension/view_history';
-    const headers = {
-      'Content-Type': 'application/json',
-      'Access-Control-Origin': '*'
-    };
-    const user = response.user;
-    const url = response.url;
-    const input = {
-      user_id: user.user_id,
-      user_name: user.user_name,
-      url
-    };
-    console.log('history inputs: ', input);
-    const data = await fetch(api, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify(input)
-    });
-    const json = await data.json();
-    chrome.storage.local.set({ history: json }, function() {
-      console.log('history', json);
+const getURL = () => {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, ([currentTab]) => {
+      resolve(chrome.storage.local.set({ url: currentTab.url }));
     });
   });
-}
+};
 
-const crawlCandidate = async () => {
-  chrome.storage.local.get(null, async function(response) {
-    chrome.storage.local.set({ resumeCount: response.resumeCount + 1 });
-    const api = 'http://128.199.203.161:8500/extension/parsing';
-    const headers = {
-      'Content-Type': 'application/json',
-      'Access-Control-Origin': '*'
-    };
-    const input = {
-      user_id: response.user.user_id,
-      user_name: response.user.user_name,
-      url: response.url,
-      html: response.html
-    };
-    console.log('crawl inputs: ', input);
-    const data = await fetch(api, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify(input)
-    });
-    const json = await data.json();
-    chrome.storage.local.set({ candidate: json });
-    console.log(json);
-    return json;
+const getHTML = () => {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.executeScript(
+      null,
+      { code: 'var html = document.documentElement.outerHTML; html' },
+      html => {
+        resolve(chrome.storage.local.set({ html: html[0] }));
+      }
+    );
   });
 };
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+const getHistory = () => {
+  return new Promise((resolve, reject) => {
+    resolve(
+      chrome.storage.local.get(null, response => {
+        fetch('http://128.199.203.161:8500/extension/view_history', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Origin': '*'
+          },
+          body: JSON.stringify({
+            user_id: response.user.user_id,
+            user_name: response.user.user_name,
+            url: response.url
+          })
+        });
+      })
+    )
+      .then(data => data.json())
+      .then(result => chrome.storage.local.set({ history: result }))
+      .catch(error => console.log(error));
+  });
+};
+
+const crawlCandidate = () => {
+  const api = 'http://128.199.203.161:8500/extension/parsing';
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Origin': '*'
+  };
+  return new Promise((resolve, reject) => {
+    resolve(
+      chrome.storage.local.get(null, response => {
+        const input = {
+          user_id: response.user.user_id,
+          user_name: response.user.user_name,
+          url: response.url,
+          html: response.html
+        };
+        console.log('crawl inputs: ', input);
+        fetch(api, {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify(input)
+        });
+      })
+    )
+      .then(data => data.json())
+      .then(json => chrome.storage.local.set({ candidate: json.result }))
+      .catch(error => console.log(error));
+  });
+};
+
+const compileMessage = port => {
+  return new Promise((resolve, reject) => {
+    resolve(
+      chrome.storage.local.get(null, response => {
+        const message = {
+          user: response.user,
+          url: response.url,
+          html: response.html,
+          history: response.history,
+          resumeCount: response.resumeCount,
+          candidate: response.candidate
+        };
+        return message;
+      })
+    )
+      .then(message => port.postMessage(message))
+      .catch(error => console.log(error));
+  });
+};
+
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
